b5524ae10e16f73b2885a713038f9cb5fa747a99
funway <>
2020-07-27 10:00:01 +0800

    Init

---
+++ Countdown/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Countdown
+//
+//  Created by funway on 2020/7/27.
+//  Copyright © 2020 funwaywang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
